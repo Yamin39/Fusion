240d84d5df1a0c09f222c63e0a63d202f02ad6a1
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2023-12-24 02:11:34 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,59 @@
+// digital clock
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    
+    var time = h + ":" + m +  " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
+
+// cursor tail
+
+const cursor = document.querySelector(".cursor");
+const posValueX = document.querySelector(".valueOfX");
+const posValueY = document.querySelector(".valueOfY");
+var timeout;
+
+document.addEventListener("mousemove", (e) => {
+	let x = e.pageX;
+	let y = e.pageY;
+
+	cursor.style.top = y + "px";
+	cursor.style.left = x + "px";
+	cursor.style.display = "block";
+	
+    
+	// cursor tail will stop while mouse isn't moving  
+	function mouseStopped(){
+        cursor.style.display = "none";
+	}
+	// clearTimeout(timeout);
+	timeout = setTimeout(mouseStopped, -1);
+    posValueX.innerHTML = x;
+    posValueY.innerHTML = y;
+});
+
+document.addEventListener("mouseout", () => {
+	cursor.style.display = "none";
+});
