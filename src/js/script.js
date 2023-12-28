865ae3662fdbecb5245ce574c4093d4b8151f333
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2023-12-28 19:42:51 +0600

    about, card and many more

--- src/js/script.js
+++ src/js/script.js
@@ -34,44 +34,53 @@ const coords = { x: 0, y: 0 };
 const circles = document.querySelectorAll(".circle");
 const posValueX = document.querySelector(".valueOfX");
 const posValueY = document.querySelector(".valueOfY");
+let isMouseMoving = false;
+let timeoutId;
 
-document.addEventListener("mouseleave", function () {
-    gsap.to(circles,{
-      opacity:0
-    })
-  });
-window.addEventListener("mousemove", function (e) {
+document.addEventListener("mousemove", function (e) {
+  if (!isMouseMoving) {
+    gsap.to(circles, {
+      opacity: 0.25,
+    });
+  }
+
+  isMouseMoving = true;
   coords.x = e.clientX;
   coords.y = e.clientY;
-  gsap.to(circles,{
-    opacity:0.25
-  })
+
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    isMouseMoving = false;
+    gsap.to(circles, {
+      opacity: 0,
+    });
+  }, 500); // 0.5 seconds delay
 });
 
 function animateCircles() {
-    let x = coords.x;
-    let y = coords.y;
-    
-    circles.forEach(function (circle, index) {
-        circle.style.left = x - 12 + "px";
-        circle.style.top = y - 12 + "px";
-        
-        circle.style.scale = (circles.length - index) / circles.length;
-        
-        circle.x = x;
-        circle.y = y;
-        
-        const nextCircle = circles[index + 1] || circles[0];
-        x += (nextCircle.x - x) * 0.3;
-        y += (nextCircle.y - y) * 0.3;
-    });
-    
-    requestAnimationFrame(animateCircles);
+  let x = coords.x;
+  let y = coords.y;
 
-    posValueX.innerHTML = x;
-    posValueY.innerHTML = y;
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+
+  posValueX.innerHTML = x;
+  posValueY.innerHTML = y;
 }
-    
+
 animateCircles();
 
 // scroll animation
